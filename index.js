d96ce61f0285a13877c0442dbfd8f33bcaa1f3b8
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-11-08 18:42:01 +0600

    mongodb user created

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -8,6 +9,21 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rzk36ti.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+run().catch( error => console.error(error))
 
 app.get('/', (req, res) => {
     res.send('FOODIGO server is running')
