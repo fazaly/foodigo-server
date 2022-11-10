2a908e2f4553b4dd3c307671e7b29a05de5571ae
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-11-10 10:59:11 +0600

    review update

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
+// json web token
+const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -21,6 +24,32 @@ async function run(){
         const serviceCollection = client.db('FOODIGO').collection('services');
 
         const reviewCollection = client.db('FOODIGO').collection('reviews');
+
+        // json web token
+        function verifyJWT(req, res, next){
+            // console.log(req.headers.authorization)
+            const authHeader = req.headers.authorization;
+            if(!authHeader){
+                return res.status(401).send({message: 'unauthorized access'})
+            }
+            const token = authHeader.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(error, decoded) {
+                if(error){
+                    return res.status(403).send({message: 'Forbidden access'})
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+         // create json web token API 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            // console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+            // if you want to send token as a actual json you have to convert it as an object
+            res.send({token});
+        })
         
         // create services API
         app.post('/services', async(req, res) => {
@@ -68,6 +97,23 @@ async function run(){
 
         // read data
         app.get('/reviews', async(req, res) => {
+
+            // // json web token
+            // console.log(req.headers.authorization);
+            // const decoded = req.decoded;
+            // console.log('inside reviews API', decoded);
+            // if(decoded.email !== req.query.email){
+            //     res.status(403).send({message: 'unauthorized access'})
+            // }
+
+            // // console.log(req.query.email);
+            // let query = {};
+            // // loading data by using query parameters
+            // if(req.query.email){
+            //     query = {
+            //         email: req.query.email
+            //     }
+            // }
             const query = {}
             const cursor = reviewCollection.find(query);
             const reviews = await cursor.toArray();
@@ -75,15 +121,38 @@ async function run(){
         });
 
         app.get('/review', async (req, res) => {
-            let query = {}
-            if (req.query.email) {
-                query = {
-                    email: req.query.email
-                }
-            }
+            const query = {}
             const cursor = reviewCollection.find(query)
             const review = await cursor.toArray()
             res.send(review)
+        });
+
+        // delete single data
+        app.delete('/review/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const remain = await reviewCollection.deleteOne(query)
+            res.send(remain)
+        });
+
+        // Update data
+        app.get("/myreviews/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewCollection.findOne(query)
+            res.send(result)
+        });
+
+        // update 
+        app.patch('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = req.body;
+            const filter = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: query
+            }
+            const result = await reviewCollection.updateOne(filter, updateDoc)
+            res.send(result);
         });
     }
     finally{
